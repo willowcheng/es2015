5d943e650f0c8d53f67103fe41fe92e9e8920aca
willowcheng <e.wingus@gmail.com>
2017-03-14 15:07:42 -0400

    Object property shorthand

---
+++ object-property-shorthand/shorthand.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function submit(name, comments, rating = 5) {
+  let data = { name, comments, rating };
+  
+  for (let key in data) {
+    console.log(key + ':', data[key]);
+  }
+  // … do ajax request
+}
+              
+//
+submit('English', 'Great course!', 9);
